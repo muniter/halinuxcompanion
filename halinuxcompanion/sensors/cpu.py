6666493bb0a50baeb272a28d9e0338a98427aa3f
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-02-18 20:50:32 +0200

    Set CPU/memory sensors to unavailable while suspended or powered off

--- halinuxcompanion/sensors/cpu.py
+++ halinuxcompanion/sensors/cpu.py
@@ -4,6 +4,7 @@ import psutil
 import os
 
 load_average: bool = False
+allow_update: bool = True
 
 Cpu = Sensor()
 Cpu.config_name = "cpu"
@@ -25,7 +26,38 @@ Cpu.unique_id = "cpu_load"
 Cpu.unit_of_measurement = "%"
 
 
+async def on_prepare_for_sleep(self, v):
+    """Handler for system sleep and wake up from sleep events.
+    https://www.freedesktop.org/software/systemd/man/org.freedesktop.login1.html
+
+    :param v: True if going to sleep, False if waking up from it
+    """
+    global allow_update
+    if v:
+        allow_update = False
+        self.state = "unavailable"
+    else:
+        allow_update = True
+
+
+async def on_prepare_for_shutdown(self, v):
+    """Handler for system shutdown/reboot.
+    https://www.freedesktop.org/software/systemd/man/org.freedesktop.login1.html
+
+    :param v: True if shutting down, False if powering on.
+    """
+    global allow_update
+    if v:
+        allow_update = False
+        self.state = "unavailable"
+    else:
+        allow_update = True
+
+
 def updater(self):
+    if not allow_update:
+        return
+
     self.state = psutil.cpu_percent()
     if load_average:
         data = psutil.getloadavg()
@@ -35,3 +67,7 @@ def updater(self):
 
 
 Cpu.updater = MethodType(updater, Cpu)
+Cpu.signals = {
+    "system.login_on_prepare_for_sleep": on_prepare_for_sleep,
+    "system.login_on_prepare_for_shutdown": on_prepare_for_shutdown,
+}
--- halinuxcompanion/sensors/memory.py
+++ halinuxcompanion/sensors/memory.py
@@ -2,6 +2,8 @@ from types import MethodType
 from halinuxcompanion.sensor import Sensor
 import psutil
 
+allow_update: bool = True
+
 Memory = Sensor()
 Memory.config_name = "memory"
 Memory.attributes = {
@@ -21,7 +23,38 @@ Memory.unique_id = "memory_usage"
 Memory.unit_of_measurement = "%"
 
 
+async def on_prepare_for_sleep(self, v):
+    """Handler for system sleep and wake up from sleep events.
+    https://www.freedesktop.org/software/systemd/man/org.freedesktop.login1.html
+
+    :param v: True if going to sleep, False if waking up from it
+    """
+    global allow_update
+    if v:
+        allow_update = False
+        self.state = "unavailable"
+    else:
+        allow_update = True
+
+
+async def on_prepare_for_shutdown(self, v):
+    """Handler for system shutdown/reboot.
+    https://www.freedesktop.org/software/systemd/man/org.freedesktop.login1.html
+
+    :param v: True if shutting down, False if powering on.
+    """
+    global allow_update
+    if v:
+        allow_update = False
+        self.state = "unavailable"
+    else:
+        allow_update = True
+
+
 def updater(self):
+    if not allow_update:
+        return
+
     data = psutil.virtual_memory()
     self.state = round((data.total - data.available) / data.total * 100, 1)
     self.attributes["total"] = data.total / 1024
@@ -31,3 +64,7 @@ def updater(self):
 
 
 Memory.updater = MethodType(updater, Memory)
+Memory.signals = {
+    "system.login_on_prepare_for_sleep": on_prepare_for_sleep,
+    "system.login_on_prepare_for_shutdown": on_prepare_for_shutdown,
+}
